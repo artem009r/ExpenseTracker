cc4c9db335b50240f030f58311c4f758f599d41a
artem009r <arsenevartem2009@mail.ru>
2025-07-06 16:56:05 +0200

    Update main.cpp, to store the current user and show a greeting after login.

--- main.cpp
+++ main.cpp
@@ -6,53 +6,91 @@
 
 using namespace std;
 
-
 int main() 
 {
     cout << "=== Система учёта расходов и доходов ===\n";
 
+    User currentUser; 
+    bool isLoggedIn = false;
     bool isRunning = true;
+
     while (isRunning) 
     {
         cout << "\nГлавное меню:\n";
-        cout << "1. Вход\n";
-        cout << "2. Регистрация\n";
-        cout << "3. Выход\n";
-        cout << "Выберите действие (1-3): ";
+        if (isLoggedIn) 
+        {
+            cout << "Вы вошли как: " << currentUser.login << "\n";
+            cout << "1. Выход из аккаунта\n";
+            cout << "2. Завершить программу\n";
+        }
+        else 
+        {
+            cout << "1. Вход\n";
+            cout << "2. Регистрация\n";
+            cout << "3. Выход\n";
+        }
+        cout << "Выберите действие: ";
 
         int choice;
         cin >> choice;
-        cin.ignore(); 
+        cin.ignore();
 
-        switch (choice) 
+        if (isLoggedIn) 
         {
-        case 1: {
-            // Вход
-            string login, password;
-            cout << "Введите логин: ";
-            getline(cin, login);
-            cout << "Введите пароль: ";
-            getline(cin, password);
-            loginUser(login, password);
-            break;
+            switch (choice) 
+            {
+            case 1:
+                isLoggedIn = false;
+                currentUser = User();
+                cout << "Вы вышли из аккаунта.\n";
+                break;
+            case 2:
+                cout << "До свидания!\n";
+                isRunning = false;
+                break;
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+                break;
+            }
         }
-        case 2: {
-            // Регистрация
-            string login, password;
-            cout << "Введите логин: ";
-            getline(cin, login);
-            cout << "Введите пароль: ";
-            getline(cin, password);
-            registerUser(login, password);
-            break;
-        }
-        case 3:
-            cout << "До свидания!\n";
-            isRunning = false;
-            break;
-        default:
-            cout << "Неверный выбор. Попробуйте снова.\n";
-            break;
+        else 
+        {
+            switch (choice) 
+            {
+            case 1: 
+            {
+                // Вход
+                string login, password;
+                cout << "Введите логин: ";
+                getline(cin, login);
+                cout << "Введите пароль: ";
+                getline(cin, password);
+                currentUser = loginUser(login, password);
+                if (!currentUser.login.empty()) 
+                {
+                    isLoggedIn = true;
+                }
+                break;
+            }
+            case 2: 
+            {
+                // Регистрация
+                string login, password;
+                cout << "Введите логин: ";
+                getline(cin, login);
+                cout << "Введите пароль: ";
+                getline(cin, password);
+                registerUser(login, password);
+                break;
+            }
+            case 3:
+                cout << "До свидания!\n";
+                isRunning = false;
+                break;
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+                break;
+            }
         }
     }
 
